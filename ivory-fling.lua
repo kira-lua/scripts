4e0d3f611bb3782e19e16796a1a832821e21d185
kira-lua <sarahadhx@gmail.com>
2026-08-23 08:47:08 +0200

    Update ivory-fling.lua

--- ivory-fling.lua
+++ ivory-fling.lua
@@ -19,12 +19,11 @@ corner.Parent = btn
 local flag = 0
 
 btn.MouseButton1Click:Connect(function()
-    if flag == 1 then return end
     flag = 1
     local char = player.Character
     local tool = player.Backpack:FindFirstChild("IvoryPeriastron")
 
-    if tool then
+    if tool and flag then
         local weaponAlreadyEquipped
         local weaponsList = {"SteampunkGlove", "Taser", "ChartreusePeriastron", "FallPeriastron", "LinkedSword", "8BitSword", "PotOfGoldSword", "FuseBomb", "NeonSpaceGun", "RedHyperLaser", "LaserFingerPointers", "Acceleration Coil", "AmethystPeriastron", "AzurePeriastron", "ChristmasTreeSword", "CrimsonPeriastron", "FestivePeriastron", "FireSword", "GrimgoldPeriastron", "IvoryPeriastron", "RainbowPeriastron", "RocketBoots", "WaterSword"}
         local carpetsList = {"GoldenCarpet", "RainbowMagicCarpet"}
@@ -34,15 +33,12 @@ btn.MouseButton1Click:Connect(function()
                 if obj.Name == e then
                     weaponAlreadyEquipped = obj.Name
                     local w = char:FindFirstChild(weaponAlreadyEquipped)
-                    if w then
-                        w.Parent = player.Backpack
-                    end
+                    w.Parent = player.Backpack
                 end
             end
             for _, e in ipairs(carpetsList) do
                 if obj.Name == e then
-                    -- On garde le tapis équipé pour ne pas qu'il disparaisse visuellement
-                    weaponAlreadyEquipped = obj.Name 
+                    weaponAlreadyEquipped = obj.Name
                 end
             end
         end
@@ -50,16 +46,18 @@ btn.MouseButton1Click:Connect(function()
         tool.Parent = char
         local remote = tool:WaitForChild('Remote')
         
-        remote:FireServer(Enum.KeyCode.E)    
-        remote:FireServer(Enum.KeyCode.E)    
+            remote:FireServer(Enum.KeyCode.E)    
+        
+        local remote = tool:WaitForChild('Remote')
+        
+            remote:FireServer(Enum.KeyCode.E)    
+        
 
         tool.Parent = player.Backpack
-
-        -- Si c'était une arme, on la remet. Si c'était un tapis, on s'assure qu'il reste bien affiché/géré
-        if weaponAlreadyEquipped then
-            local item = player.Backpack:FindFirstChild(weaponAlreadyEquipped) or char:FindFirstChild(weaponAlreadyEquipped)
-            if item and weaponAlreadyEquipped ~= 'RainbowMagicCarpet' and weaponAlreadyEquipped ~= 'GoldenCarpet' then
-                item.Parent = char
+        if weaponAlreadyEquipped and weaponAlreadyEquipped ~= 'RainbowMagicCarpet' and weaponAlreadyEquipped ~= 'GoldenCarpet' then
+            local weapon = player.Backpack:FindFirstChild(weaponAlreadyEquipped)
+            if weapon then
+                weapon.Parent = char
             end
         end
     end
